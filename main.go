30bcb1c2ca22d49bb9c304dbd9570f501bff6989
thomas <zhangfan@thomas-fan.com>
2021-02-02 10:07:51 +0800

    调整 gin 初始化代码

--- main.go
+++ main.go
@@ -2,13 +2,16 @@ package main
 
 import "github.com/gin-gonic/gin"
 
-func main() {
+func setupRouter() *gin.Engine {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-
+	return r
+}
+func main() {
+	r := setupRouter()
 	r.Run("0.0.0.0:8888")
 }
